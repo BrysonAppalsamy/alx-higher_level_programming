442fc70e703214da987d377dcafc71a789a90811
BrysonAppalsamy <brysonappalsamy@gmail.com>
2024-02-27 00:58:26 -0800

    loops && functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number < 0:
-    num = number * -1
-    num = num % 10
-    num = num * -1
+if number >= 0:
+    l_digit = number % 10
 else:
-    num = number % 10
-print("Last digit of {}".format(number), end=' ')
-if num > 5:
-    print("is {} and is greater than 5".format(num))
-elif num == 0:
-    print("is {} and is 0".format(num))
-elif num < 6 and num != 0:
-    print("is {} and is less than 6 and not 0".format(num))
+    l_digit = number % -10
+
+print("Last digit of {} is {}".format(number, l_digit), end='')
+
+if l_digit > 5:
+    print(" and is greater than 5")
+elif l_digit == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}, ".format(i, j), end='')
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
